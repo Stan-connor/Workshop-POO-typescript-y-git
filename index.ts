b9b3243f9513d35faddc514efd1a4a0b3ed946d9
Black-Men <120544713+Stan-connor@users.noreply.github.com>
2024-10-01 21:52:51 -0500

    4.Ejemplo, rectificacion del ejercicio

--- index.ts
+++ index.ts
@@ -1,41 +1,32 @@
 import { Persona } from "./Persona";
+import { Empleado } from "./Empleado";
 
-    const Persona1 = new Persona(`pedro`, 15, `hola`);
+    const Persona1 = new Persona(`pedro`, 22, `hola`);
         Persona1.saludar();
-            console.log(`Edad de ${Persona1.nombre}: ${Persona1.getEdad()} años.`);
 
-    const Persona2 = new Persona(`luisa`, 15, `hola`);
+    const Persona2 = new Persona(`luisa`, 25, `hola`);
             Persona2.saludar();
-                console.log(`Edad de ${Persona2.nombre}: ${Persona2.getEdad()} años.`);
 
-    const Persona3 = new Persona(`camila`, 13, `hola`);
+    const Persona3 = new Persona(`camila`, 23, `hola`);
             Persona3.saludar();
-                console.log(`Edad de ${Persona3.nombre}: ${Persona3.getEdad()} años.`);
 
-    const Persona4 = new Persona(`david`, 16, `hola`);
-            Persona4.saludar();
-                console.log(`Edad de ${Persona1.nombre}: ${Persona1.getEdad()} años.`);
+    const Persona4 = new Persona(`david`, 26, `hola`);
+            Persona4.saludar()
 
-    const Persona5 = new Persona(`carlos`, 17, `hola`);
+    const Persona5 = new Persona(`carlos`, 27, `hola`);
             Persona5.saludar();
-                console.log(`Edad de ${Persona5.nombre}: ${Persona5.getEdad()} años.`);
-
-    const Persona6 = new Persona(`laura`, 18, `hola`);
+            
+    const Persona6 = new Persona(`laura`, 28, `hola`);
             Persona6.saludar();
-                console.log(`Edad de ${Persona6.nombre}: ${Persona6.getEdad()} años.`);
 
-    const Persona7 = new Persona(`camilo`, 16, `hola`);
+    const Persona7 = new Persona(`camilo`, 26, `hola`);
             Persona7.saludar();
-                console.log(`Edad de ${Persona7.nombre}: ${Persona7.getEdad()} años.`);
 
-    const Persona8 = new Persona(`felipe`, 12, `hola`);
+    const Persona8 = new Persona(`felipe`, 22, `hola`);
             Persona8.saludar();
-                console.log(`Edad de ${Persona8.nombre}: ${Persona8.getEdad()} años.`);
 
-    const Persona9 = new Persona(`yonier`, 19, `hola`);
+    const Persona9 = new Persona(`yonier`, 29, `hola`);
             Persona9.saludar();
-                console.log(`Edad de ${Persona9.nombre}: ${Persona9.getEdad()} años.`);
 
-    const Persona10 = new Persona(`isabel`, 17, `hola`);
+    const Persona10 = new Persona(`isabel`, 27, `hola`);
             Persona10.saludar();
-                console.log(`Edad de ${Persona10.nombre}: ${Persona10.getEdad()} años.`);
